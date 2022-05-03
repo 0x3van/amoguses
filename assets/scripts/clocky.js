1e54970513872c726e04a4409abff0ad969bb329
evan <97516411+0x3van@users.noreply.github.com>
2022-05-03 23:01:44 +0800

    Add files via upload

---
+++ assets/scripts/clocky.js
@@ -0,0 +1,19 @@
+function updateClock () { 
+ 	let currentTime = new Date ();
+  	let currentHours = currentTime.getHours ( );
+  	let currentMinutes = currentTime.getMinutes ( );
+  	let currentSeconds = currentTime.getSeconds ( );
+
+  	currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+  	currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+  	let timeOfDay = ( currentHours < 12 ) ? "am" : "pm";
+
+  	currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+
+  	currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+
+  	let currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+ 
+   	$(".time").html(currentTimeString);
+}
